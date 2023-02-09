6d2160ec11bde7ff9be5fe230e17ae03c29dc85b
Rakesh Vidyadharan <rakesh@sptci.com>
2023-02-09 11:01:52 -0600

    Enable LTO.

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-DIR=`dirname $0`
-. $DIR/env.sh
+cd `dirname $0`/..
+. docker/env.sh
 
 Docker()
 {
-  (cd $DIR/..;
-    docker build --compress --force-rm -f docker/Dockerfile -t $NAME .)
+  docker buildx build --platform linux/amd64 --compress --force-rm -f docker/Dockerfile --push -t sptrakesh/$NAME:$VERSION .
+  docker pull sptrakesh/$NAME:$VERSION
+  docker tag sptrakesh/$NAME:$VERSION $NAME
+  docker tag sptrakesh/$NAME:$VERSION sptrakesh/$NAME:latest
+  docker push sptrakesh/$NAME:latest
+  docker buildx build --platform linux/arm64,linux/amd64 --compress --force-rm -f docker/Dockerfile --push -t sptrakesh/$NAME:$VERSION .
 }
 
 Docker
